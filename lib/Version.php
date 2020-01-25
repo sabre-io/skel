bbbf3b956e63058ed3e6092941399b2e7c85ea68
Phil Davis <phil@jankaritech.com>
2020-01-25 21:22:37 +0545

    implement php-cs-fixer

--- lib/Version.php
+++ lib/Version.php
@@ -1,4 +1,6 @@
-<?php declare (strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Sabre\Skel;
 
@@ -9,11 +11,10 @@ namespace Sabre\Skel;
  * @author Evert Pot (http://evertpot.com/)
  * @license http://sabre.io/license/
  */
-class Version {
-
+class Version
+{
     /**
-     * Full version number
+     * Full version number.
      */
     const VERSION = '0.0.1';
-
 }
